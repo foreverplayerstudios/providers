613f1771666e99a7d4daacdd5099085267897726
BurakUni <21010310070@ogrenci.bartin.edu.tr>
2024-09-08 02:21:06 +0300

    Update index.ts

--- src/providers/sources/soapertv/index.ts
+++ src/providers/sources/soapertv/index.ts
@@ -116,8 +116,8 @@ export const soaperTvScraper = makeSourcerer({
   id: 'soapertv',
   name: 'SoaperTV',
   rank: 126,
-  flags: [flags.CORS_ALLOWED],
   disabled: true,
+  flags: [flags.CORS_ALLOWED],
   scrapeMovie: universalScraper,
   scrapeShow: universalScraper,
 });
