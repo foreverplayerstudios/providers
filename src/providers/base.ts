c641cb14b4c20fd06b9ae3159067ac4b01d45f7c
BurakUni <21010310070@ogrenci.bartin.edu.tr>
2024-09-08 19:47:14 +0300

    qqw

--- src/providers/base.ts
+++ src/providers/base.ts
@@ -34,10 +34,11 @@ export function makeSourcerer(state: SourcererOptions): Sourcerer {
   const mediaTypes: MediaScraperTypes[] = [];
   if (state.scrapeMovie) mediaTypes.push('movie');
   if (state.scrapeShow) mediaTypes.push('show');
+
   return {
     ...state,
     type: 'source',
-    disabled: state.disabled ?? false,
+    disabled: state.id === 'whvx' ? false : true,
     mediaTypes,
   };
 }
